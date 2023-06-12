543193545e42788d4fe769e32f4a77f7ae026694
AngelEscritorio <angelcursosteam@hotmail.com>
2023-06-11 21:00:57 -0600

    proceso de ejercicio

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+const name = 'vue dinamico';
+
+//netidi - methods
+const handleClick= (message) => {
+  console.log (message)
+}
+</script>
+
+<template>
+  <h1>Hola {{ name.toUpperCas }}</h1>
+  <button v-on:click="handleClick('Texto 1')">Activame 1</button>
+  <button @click="handleClick('Texto 2')">Activame 2</button>
+</template>
+
+<style>
+h1 {
+  color: red
+}
+</style>
