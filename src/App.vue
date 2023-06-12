6d3a58b3969b7a6bf5c97fda77a723dbb827c63a
AngelEscritorio <angelcursosteam@hotmail.com>
2023-06-11 21:53:05 -0600

    proceso de ejercicio

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <script setup>
+import {ref} from 'vue'
 const name = 'vue dinamico';
 
 //netidi - methods
-const handleClick= (message) => {
-  console.log (message)
+const counter = ref (0)
+const incremento = () => {
+  console.log('aumentar contador')
+  counter.value++;
+  console.log(counter);
+}
+
+const reduccion = () => {
+  console.log('reducir contador')
+  counter.value--;
+  console.log(counter);
+}
+
+const reseteo = () => {
+  console.log('resetear contador')
+  counter.value = 0;
+  console.log(counter);
 }
 </script>
 
 <template>
   <h1>Hola {{ name.toUpperCas }}</h1>
-  <button v-on:click="handleClick('Texto 1')">Activame 1</button>
-  <button @click="handleClick('Texto 2')">Activame 2</button>
+  <h2 v-if="counter < 0">
+    <p style="color: red">{{ counter }}</p>
+  </h2>
+  <h2 v-if="counter > 0">
+    <p style="color: green">{{ counter }}</p>
+  </h2>
+  <h2 v-if="counter == 0">
+    <p style="color: gray">{{ counter }}</p>
+  </h2>
+  <button @click="incremento">Aumentar</button>
+  <button @click="reduccion">Reducir</button>
+  <button @click="reseteo">Resetear</button>
+  
+
 </template>
 
 <style>
